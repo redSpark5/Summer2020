dee615eed870bfd33f07b127a9f81f647412f2da
Jessica A <bunting990@gmail.com>
2020-07-29 20:46:54 -0500

    Saving July 29, 2020

---
+++ readme.txt
@@ -0,0 +1,18 @@
+/*--- weather-app ------------------------------------------------
+Programmer: Jessica Adamec
+Date: 7-1-2020
+
+This application makes an API request to OpenWeatherMap.org. It   
+returns the current temperature of the user-entered city. 
+
+This application was created as a learning exercise during my summer 
+2020 internship with RBC Wealth Management. This application represents
+some of the things I learned during my internship, such as API's, 
+how to make API calls, and requests and responses. 
+
+This project was created with the help of the following website:
+https://codeburst.io/build-a-weather-website-in-30-minutes-with-node-js-express-openweather-a317f904897b
+-------------------------------------------------------------------*/
+
+
+
---
+++ server.js
@@ -0,0 +1,43 @@
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express()
+
+//Key 
+const apiKey = '43a0da681078b11116b395bba0c48876';
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+})
+
+//Making a request to the OpenWeatherMap API
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+  //Check for an error first
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        //Return the information
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
+
+app.listen(3000, function () {
+  console.log('Listening on port: 3000')
+})
